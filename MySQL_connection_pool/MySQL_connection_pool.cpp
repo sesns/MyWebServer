7a6cdf5ceddc5c465f958565382662a1de8afac0
sesns <1165744631@qq.com>
2022-05-03 22:18:28 +0800

    add mysql_connection_pool files

---
+++ MySQL_connection_pool/MySQL_connection_pool.cpp
@@ -0,0 +1,85 @@
+#include "MySQL_connection_pool.h"
+#include<queue>
+#include<mysql.h>
+#include<vector>
+using namespace std;
+
+void MySQL_connection_pool::init(size_t max_con_num,string host,string username,string password,string DBname,unsigned int port)
+{
+    m_max_con_num=max_con_num;
+    m_host=host;
+    m_username=username;
+    m_password=password;
+    m_DBname=DBname;
+    m_port=port;
+
+    for(int i=0;i<max_con_num;i++)
+    {
+        MYSQL* conn_ptr;
+        conn_ptr=mysql_init(conn_ptr);
+        if(!conn_ptr)
+        {
+            cout<<"Error:"<<mysql_error(conn_ptr)<<"\n";
+            exit(1);
+        }
+
+        conn_ptr=mysql_real_connect(conn_ptr,m_host.c_str(),m_username.c_str(),m_password.c_str(),m_DBname.c_str(),m_port,NULL,0);
+        if(!conn_ptr)
+        {
+            cout<<"MySQL Connection failed:"<<mysql_error(conn_ptr)<<"\n";
+            exit(1);
+        }
+
+        m_queue.push(conn_ptr);
+        m_conns.push_back(conn_ptr);
+        m_free_con_num+=1;
+
+    }
+
+    xinhaoliang=sem(m_free_con_num);
+}
+
+void MySQL_connection_pool::DestroyPool()//销毁连接池
+{
+    loc.lock();
+    for(int i=0;i<m_conns.size();i++)
+    {
+        MYSQL* con=m_conns[i];
+        mysql_close(con);
+    }
+
+    loc.unlock();
+}
+
+MYSQL* MySQL_connection_pool::getCon()//获取连接
+{
+    xinhaoliang.wait();
+
+    loc.lock();
+
+    MYSQL* con=m_queue.front();
+    m_queue.pop();
+    m_free_con_num--;
+    m_used_con_num++;
+
+    loc.unlock();
+
+    return con;
+}
+
+bool MySQL_connection_pool::releaseCon(MYSQL* conn)//释放连接
+{
+    if(!conn)
+        return NULL;
+
+    loc.lock();
+
+    m_queue.push(conn);
+    m_free_con_num++;
+    m_used_con_num--;
+
+    loc.unlock();
+
+    xinhaoliang.post();
+    return true;
+}
---
+++ MySQL_connection_pool/MySQL_connection_pool.h
@@ -0,0 +1,75 @@
+#ifndef MYSQL_CONNECTION_POOL_H_INCLUDED
+#define MYSQL_CONNECTION_POOL_H_INCLUDED
+#include<queue>
+#include<mysql.h>
+#include<vector>
+#include"Locker.h"
+using namespace std;
+class MySQL_connection_pool
+{
+private:
+    MySQL_connection_pool()
+    {
+        m_free_con_num=0;
+        m_used_con_num=0;
+    }
+    ~MySQL_connection_pool()
+    {
+        DestroyPool();
+    }
+public:
+    MySQL_connection_pool(const MySQL_connection_pool& mcp)=delete;
+    MySQL_connection_pool(MySQL_connection_pool&& mcp)=delete;
+    MySQL_connection_pool& operator=(const MySQL_connection_pool& mcp)=delete;
+    MySQL_connection_pool& operator=(MySQL_connection_pool&& mcp)=delete;
+    MySQL_connection_pool* getInstance()//单例模式
+    {
+        static MySQL_connection_pool m_instance;
+        return &m_instance;
+    }
+private:
+    void DestroyPool();//销毁连接池
+    size_t m_max_con_num;//最大连接数
+    size_t m_free_con_num;//当前空闲连接数
+    size_t m_used_con_num;//当前已使用连接数
+    queue<MYSQL*> m_queue;//连接池
+    vector<MYSQL*> m_conns;//保存所有连接的指针，将来用户释放连接池
+
+    sem xinhaoliang;//信号量
+    locker loc;//互斥锁
+
+    string m_host;//主机名
+    string m_username;//用户名
+    string m_password;//密码
+    string m_DBname;//要访问的数据库的名字
+    unsigned int m_port;//端口号
+public:
+    void init(size_t max_con_num,string host,string username,string password,string DBname,unsigned int port);
+    MYSQL* getCon();//获取连接
+    bool releaseCon(MYSQL* conn);//释放连接
+    size_t get_freecon_num()//获取当前空闲连接个数
+    {
+        return m_free_con_num;
+    }
+
+};
+
+class MySQLconRAII
+{
+private:
+    MySQL_connection_pool* m_mysql_con_pool;
+    MYSQL* m_con;
+public:
+    MySQLconRAII(MYSQL** con,MySQL_connection_pool* mysql_con_pool)
+    {
+        m_mysql_con_pool=mysql_con_pool;
+        m_con=m_mysql_con_pool->getCon();
+
+        (*con)=m_con;
+    }
+    ~MySQLconRAII()
+    {
+        m_mysql_con_pool->releaseCon(m_con);
+    }
+};
+#endif // MYSQL_CONNECTION_POOL_H_INCLUDED
